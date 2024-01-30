c687a299e162ae63e87a1b6d596dc273aca9d681
Benjamin O'Neill <tun49498@temple.edu>
2024-01-30 12:14:34 -0500

    Constrained the layout Initialized Views

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,17 +10,21 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
-
+    lateinit var editText: EditText
+    lateinit var clickMeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        editText = findViewById(R.id.nameEditText)
+        clickMeButton = findViewById(R.id.clickMeButton)
 
         
         // Respond to button click event per specifications
 
 
     }
+
 }
